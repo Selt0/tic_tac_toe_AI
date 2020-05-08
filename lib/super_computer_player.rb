ff9839ceaa5303aa076eeea368ef2930d347002e
Selt0 <chewedgum12@gmail.com>
2020-05-08 11:45:12 -0500

    Add method move that selects a winning move or a non losing move from the list of nodes created using TicTacToeNode

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -2,6 +2,24 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    # build a TicTacToeNode from the board stored in the game passed in as an argument.
+    node = TicTacToeNode.new(game.board, mark)
+    
+    possible_moves = node.children
+
+    # iterate through the children of the node. If any of the children is a winning_node? for the mark passed in to the move method, return that node's prev_move_pos
+    node = possible_moves.find { |child| child.winning_node?(mark) }
+
+    return node.prev_move_pos if node
+
+    # If none of the children of the node are winning_node?s, that's ok. Just pick one that isn't a losing_node? and return its prev_move_pos.
+
+    node = possible_moves.find { |child| !child.losing_node?(mark) }
+
+    return node.prev_move_pos if node
+
+    # raise an error if there are no non-losing nodes
+    raise "Oh crap, you beat the computer!"
   end
 end
 
